b4c3489d0ac3265fa62fb4975865add37dddfb90
dehdennoh <ddennoh958@gmail.com>
2024-04-25 16:19:49 +0300

    initial commit

---
+++ app/src/main/java/com/example/wazitoecommerce/ui/theme/screens/home/HomeScreen.kt
@@ -0,0 +1,54 @@
+package com.example.wazitoecommerce.ui.theme.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.wazitoecommerce.R
+import com.example.wazitoecommerce.navigation.ADD_PRODUCTS_URL
+import com.example.wazitoecommerce.navigation.VIEW_PRODUCTS_URL
+import com.example.wazitoecommerce.ui.theme.WazitoECommerceTheme
+
+@Composable
+fun HomeScreen(navController:NavHostController){
+    Column(
+        modifier = Modifier.paint(
+            painterResource(id = R.drawable.img),
+            contentScale = ContentScale.FillBounds
+        )
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Plasma Link",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Serif
+        )
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HomeScreenPreview(){
+    WazitoECommerceTheme {
+        HomeScreen(navController = rememberNavController())
+    }
+}
